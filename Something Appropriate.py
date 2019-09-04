1acb25fe162a5a7f98396b7df12bfacec1b8823f
frankb <frankb@berea.edu>
2019-09-04 09:06:51 -0400

    Created Programs that draws initials MF, and Commented

--- Something Appropriate.py
+++ Something Appropriate.py
@@ -1,35 +1,42 @@
+#######################################################################
+# Set up Turtle
 import turtle
-
 wn = turtle.Screen()
 wn.bgcolor("black")
 
+#######################################################################
+# Define a new turtle object named baby
 baby = turtle.Turtle()
 baby.color("aquamarine4")
 baby.pensize(30)
 baby.speed(0)
 
+# navigate baby toward initial drawing point
 baby.penup()
 baby.backward(250)
 baby.right(90)
 baby.forward(100)
 baby.right(180)
 baby.pendown()
+########################################################################
+# first leg of M
 baby.forward(190)
-
-
+# make the first curve for M
 for first_curve in range(60):
     baby.forward(3)
     baby.right(3)
-
+# middle leg of M
 baby.forward(80)
 baby.right(180)
 baby.forward(80)
-
+# second curve for M
 for second_curve in range(60):
     baby.forward(3)
     baby.right(3)
-
+# last leg for M
 baby. forward(190)
+#########################################################################
+# set up turtle and draw F
 baby.left(90)
 baby.penup()
 baby.forward(100)
@@ -44,5 +51,5 @@ baby.left(90)
 baby.forward(100)
 baby.right(90)
 baby.forward(140)
-
+##########################################################################
 wn.exitonclick()
